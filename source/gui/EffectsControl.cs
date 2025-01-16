e90fe381416ee8b6d4969a56a90d74f0577cf59c
Luis <104567400+Surdavow@users.noreply.github.com>
2025-01-16 15:52:04 -0800

    Refactor volume settings

--- source/gui/EffectsControl.cs
+++ source/gui/EffectsControl.cs
@@ -3,9 +3,7 @@ using GodotPlugins.Game;
 using System;
 
 public partial class EffectsControl : Control
-{	
-	public float MasterVolumeTarget;
-	public float MusicVolumeTarget;
+{
 	public float AudioLowPassTarget = 20500;
 	public string TransitionTo;
 	public TransitionRect TransitionRect;
@@ -16,56 +14,47 @@ public partial class EffectsControl : Control
 	{		
 		TransitionRect = GetNode<TransitionRect>("TransitionRect");
 
-		// Set initial volume targets
-		MasterVolumeTarget = AudioServer.GetBusVolumeDb(AudioServer.GetBusIndex("Master"));
-		MusicVolumeTarget = AudioServer.GetBusVolumeDb(AudioServer.GetBusIndex("Music"));
-
 		SoundPlayer = GetNode<EffectsSoundPlayer>("EffectsSoundPlayer");
 		MusicPlayer = GetNode<EffectsMusicPlayer>("EffectsMusicPlayer");
 	}
 
 	public override void _Process(double delta)
 	{
-	    // Handle low pass filter interpolation for music
-		AudioEffect MusicLowPass = AudioServer.GetBusEffect(AudioServer.GetBusIndex("Music"), 0);
-	    float currentCutoffHz = (float)MusicLowPass.Get("cutoff_hz");
-	    if (currentCutoffHz != AudioLowPassTarget)
-	    {
-	        float interpolatedCutoff = Mathf.Lerp(currentCutoffHz, AudioLowPassTarget, (float)delta * 2);
-	        MusicLowPass.Set("cutoff_hz", interpolatedCutoff);
-	    }
-	
-	    // Handle master volume amplify effect during transitions or normal state
-		AudioEffect MasterAmplifyFilter = AudioServer.GetBusEffect(AudioServer.GetBusIndex("Master"), 0);
-	    float TransitionRectAlphaTarget = -TransitionRect.Color.A * 40;    
-		float currentMasterAmplifyValue = (float)MasterAmplifyFilter.Get("volume_db");
-	    if (currentMasterAmplifyValue != TransitionRectAlphaTarget)
-	    {
-	        MasterAmplifyFilter.Set("volume_db",TransitionRectAlphaTarget);
-	    }
+		HandleAudioEffects(delta);
+		HandleSceneTransitions();
+	}
 
-		// Handle master volume updates
-		float currentMasterVolume = AudioServer.GetBusVolumeDb(AudioServer.GetBusIndex("Master"));
-		if (currentMasterVolume != MasterVolumeTarget)
+	private void HandleAudioEffects(double delta)
+	{
+		// Handle Low Pass Filter Interpolation
+		AudioEffect musicLowPass = AudioServer.GetBusEffect(AudioServer.GetBusIndex("Music"), 0);
+		float currentCutoffHz = (float)musicLowPass.Get("cutoff_hz");
+		if (currentCutoffHz != AudioLowPassTarget)
 		{
-			AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Master"), MasterVolumeTarget);
+			float interpolatedCutoff = Mathf.Lerp(currentCutoffHz, AudioLowPassTarget, (float)delta * 2);
+			musicLowPass.Set("cutoff_hz", interpolatedCutoff);
 		}
-	
-	    // Handle music volume updates
-	    float currentMusicVolume = AudioServer.GetBusVolumeDb(AudioServer.GetBusIndex("Music"));
-	    if (currentMusicVolume != MusicVolumeTarget)
-	    {
-	        AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Music"), MusicVolumeTarget);
-	    }
 
-		if(TransitionRect.Color.A < 0.05 && TransitionTo == "start")
+		// Handle Master Volume Amplify Effect
+		AudioEffect masterAmplifyFilter = AudioServer.GetBusEffect(AudioServer.GetBusIndex("Master"), 0);
+		float transitionRectAlphaTarget = -TransitionRect.Color.A * 40;    
+		float currentMasterAmplifyValue = (float)masterAmplifyFilter.Get("volume_db");
+		if (currentMasterAmplifyValue != transitionRectAlphaTarget)
+		{
+			masterAmplifyFilter.Set("volume_db", transitionRectAlphaTarget);
+		}
+	}
+
+	private void HandleSceneTransitions()
+	{
+		if (TransitionRect.Color.A < 0.05 && TransitionTo == "start")
 		{
 			TransitionTo = "";
 			TransitionRect.setAlpha(0, true);
 		}
-		else if(TransitionRect.Color.A > 0.995)
+		else if (TransitionRect.Color.A > 0.995)
 		{
-			switch(TransitionTo.ToLower())
+			switch (TransitionTo.ToLower())
 			{
 				case "hostgame":
 					GetTree().ChangeSceneToFile("res://resource/scenes/Game.tscn");			
--- source/gui/SettingsMenuControl.cs
+++ source/gui/SettingsMenuControl.cs
@@ -5,7 +5,6 @@ public partial class SettingsMenuControl : Control
 {
 	private Control MenuControl;
 	private EffectsControl EffectsControl;
-	private EffectsSoundPlayer EffectsSoundPlayer;
 	private MarginContainer MenuMargin;
 	public Vector2 MarginTargetPos;
 	public float lerpSpeed = 4f;
@@ -25,7 +24,6 @@ public partial class SettingsMenuControl : Control
 
 		MenuMargin.Position = new Vector2(0, -1000);
 		MarginTargetPos = MenuMargin.Position;
-		EffectsSoundPlayer = EffectsControl.GetNode<EffectsSoundPlayer>("EffectsSoundPlayer");
 	}
 
 	public override void _Process(double delta)
@@ -39,35 +37,35 @@ public partial class SettingsMenuControl : Control
 	//Universal script for hovering over a mouse button
 	public void _on_mouse_entered()
 	{
-		EffectsSoundPlayer.playStream("submenu_scroll");
+		EffectsControl.SoundPlayer.playStream("submenu_scroll");
 	}
 
 	public void _on_master_volume_slider_value_changed(float value)
 	{
-		if((string)EffectsControl.TransitionTo == "start")
-		{
-			return;
-		}
-		
-		EffectsSoundPlayer.playStream("submenu_slidein");
-		EffectsControl.MasterVolumeTarget = value;
+		setChannelVolume("Master", value);
 	}
 
 	public void _on_music_volume_slider_value_changed(float value)
 	{
+		setChannelVolume("Music", value);
+	}
+
+	public void setChannelVolume(string channel, float value)
+	{
 		if((string)EffectsControl.TransitionTo == "start")
 		{
 			return;
 		}
 
-		EffectsSoundPlayer.playStream("submenu_slidein");
-		EffectsControl.MusicVolumeTarget = value;		
+		EffectsControl.SoundPlayer.playStream("submenu_slidein");
+		AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex(channel), value);
+		AudioServer.SetBusMute(AudioServer.GetBusIndex(channel), value <= -25);
 	}
 
 	public void _on_done_button_pressed()
 	{
-		EffectsSoundPlayer.playStream("submenu_dropdown_select");
-		EffectsSoundPlayer.playStream("submenu_slidein");
+		EffectsControl.SoundPlayer.playStream("submenu_dropdown_select");
+		EffectsControl.SoundPlayer.playStream("submenu_slidein");
 		MenuControl.Set("MarginTargetPos",Vector2.Zero);
 		MarginTargetPos = new Vector2(0, -1000);
 		EffectsControl.Set("AudioLowPassTarget", 20500);
@@ -75,12 +73,12 @@ public partial class SettingsMenuControl : Control
 
 	public void _on_settings_tab_changed(int tab)	
 	{
-		EffectsSoundPlayer.playStream("submenu_select");
+		EffectsControl.SoundPlayer.playStream("submenu_select");
 	}
 
 	public void _on_resolution_slider_value_changed(float value)
 	{
-		EffectsSoundPlayer.playStream("submenu_slidein");
+		EffectsControl.SoundPlayer.playStream("submenu_slidein");
 		GetViewport().Scaling3DScale = (float)value;
 	}
 	
